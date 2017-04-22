2a96bc83ae1af30c4f002cfb9b2433e5528fec06
Joshua Safyan <jsafyan@gmail.com>
2017-04-22 17:53:01 -0400

    add test for large networks and greedy uncertainty

--- greedy_test.py
+++ greedy_test.py
@@ -7,51 +7,8 @@ import networkx as nx
 import random
 
 rules = Rules()
-rules.nb_max_step = 10
-rules.nb_players = 20
-
-game = Game()
-#game.rules = rules
-strategy_builder = StrategyBuilder()
-"""
-def create_player(name, eps=.3, delta=.3):
-    player = Player()
-    player.rules = rules
-    player.name = name
-    player.type = EntityType.competitive_player
-    player.strategy = strategy_builder.get_approx_greedy_strategy(EPSILON=eps, DELTA=delta)
-    return player
-
-def add_players(game, players):
-    for player in players:
-        game.add_player(game)
-    return game
-
-for i in range(2):
-    player = create_player("Player {}".format(i), eps=.2, delta=.05)
-    print("Created player {}".format(i))
-    game.add_player(player)
-    print("Added player {} to the game".format(i))
-
-
-game.initialize_graph()
-print("Graph initialized, starting game")
-
-for i in range(rules.nb_max_step):
-    print("Round {}".format(i))
-    game.play_round()
-
-#game.save("history/history_test.pickle")
-"""
-game.load("history/history_test.pickle")
-game.initialize_graph()
-plotter = Plotter()
-plotter.plot_game(game, interactive=False)
-
-"""
-rules = Rules()
-rules.nb_max_step = 200
-rules.nb_players = 100
+rules.nb_max_step = 2000
+rules.nb_players = 1000
 
 game = Game()
 game.rules = rules
@@ -70,7 +27,7 @@ def add_players(game, players):
         game.add_player(game)
     return game
 
-for i in range(10):
+for i in range(50):
     player = create_player("Player {}".format(i), eps=.2, delta=.05)
     print("Created player {}".format(i))
     game.add_player(player)
@@ -84,6 +41,7 @@ for i in range(rules.nb_max_step):
     print("Round {}".format(i))
     game.play_round()
 
+game.save("history/history_1000_50active.pickle")
+
 plotter = Plotter()
 plotter.plot_game(game, interactive=False)
-"""
